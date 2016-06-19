36ccfc105bc998dc9db1551946f8b8572b8af4a2
Younji <younjiwoo@gmail.com>
2016-06-19 15:09:18 -0700

    finished Assignment2 part1

---
+++ scripts/script1.js
@@ -0,0 +1,87 @@
+// step 1
+
+//var yourName = window.prompt("What's your name?");
+//window.alert(yourName.length);
+
+
+// step 2
+
+//var userName = window.prompt("What's your name?");
+//var userNumber = window.prompt("Put a number.");
+//window.alert(userName[userNumber]);
+
+
+// step 3
+
+//var userFirstName = prompt("What's your first name?");
+//var userLastName = prompt("What's your last name?");
+//alert("Your name is: " + userFirstName + " " + userLastName);
+
+
+// step 4
+
+//var someText = "The quick brown fox jumps over the lazy dog.";
+//alert(someText.indexOf('fox'));
+
+
+// step 5
+
+//var someText = "The quick brown fox jumps over the lazy fox.";
+//alert(someText.lastIndexOf('fox'));
+
+
+// step 6
+
+//var someText = "The quick brown fox jumped over the lazy dog.";
+//var userFullName = prompt("Please enter your full name.");
+//var replacedStc = someText.replace("the lazy dog", userFullName);
+//alert(replacedStc);
+
+
+// step 7
+
+//var someText = "The quick brown fox jumps over the lazy dog.";
+//var searchWord = prompt("Please enter a word.");
+//alert(someText.search(searchWord));
+
+
+// step 8
+
+//var old_string = "The quick brown fox jumps over the lazy dog";
+//var new_string = old_string.slice(31, 43);
+//var new_string = old_string.substr(31, 42);
+//alert(new_string.toUpperCase());
+
+
+// step 9
+
+//var someText = "    THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG    ";
+//alert(someText.toLowerCase().trim());
+
+
+// step 10
+var someText = "the quick brown fox jumps over the lazy dog";
+alert(someText.charAt(0).toUpperCase() + someText.slice(1));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
